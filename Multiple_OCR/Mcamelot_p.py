bbb86c66ba046ff88e4b7f687a76debb515ba6b9
Ahtsham Hussain <ahtshampowerweave@gmail.com>
2025-10-16 08:58:51 +0300

    lattice tables

--- Multiple_OCR/Mcamelot_p.py
+++ Multiple_OCR/Mcamelot_p.py
@@ -3,6 +3,9 @@ import os
 os.chdir('./')
 os.getcwd()
 
+import pandas as pd 
+print(pd.__version__)
+
 import camelot
 import pandas as pd
 lattice_tables = camelot.read_pdf('c:\\Users\\hahtsham\\work\\ICR\\Multiple_OCR\sample_with_tables.pdf', pages='all', flavor='lattice', suppress_stdout=False)
@@ -16,5 +19,4 @@ def extract_table(path):
     stream_tables = camelot.read_pdf('c:\\Users\\hahtsham\\work\\ICR\\Multiple_OCR\sample_with_tables.pdf', pages='all', flavor='stream', suppress_stdout=False)
     return lattice_tables
 
-
-main()
+ 
