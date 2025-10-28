1b57bf545fead1f932dc5a79825a81b8263babdf
Ahtsham Hussain <ahtshampowerweave@gmail.com>
2025-10-28 13:06:47 +0300

    updating llm call

--- Multiple_OCR/Mcamelot_p.py
+++ Multiple_OCR/Mcamelot_p.py
@@ -4,7 +4,7 @@ os.chdir('./')
 os.getcwd()
 
 import pandas as pd 
-print(pd.__version__)
+print('___________________________',pd.__version__)
 
 import camelot
 import pandas as pd
@@ -15,8 +15,8 @@ stream_tables = camelot.read_pdf('c:\\Users\\hahtsham\\work\\ICR\\Multiple_OCR\s
 
 def extract_table(path):
     # Lattice -> looks for clearly defined borders / lines like a grid, visible ruling lines between rows and columns
-    lattice_tables = camelot.read_pdf('c:\\Users\\hahtsham\\work\\ICR\\Multiple_OCR\sample_with_tables.pdf', pages='all', flavor='lattice', suppress_stdout=False)
-    stream_tables = camelot.read_pdf('c:\\Users\\hahtsham\\work\\ICR\\Multiple_OCR\sample_with_tables.pdf', pages='all', flavor='stream', suppress_stdout=False)
+    lattice_tables = camelot.read_pdf(path, pages='all', flavor='lattice', suppress_stdout=False)
+    #stream_tables = camelot.read_pdf('c:\\Users\\hahtsham\\work\\ICR\\Multiple_OCR\sample_with_tables.pdf', pages='all', flavor='stream', suppress_stdout=False)
     return lattice_tables
 
  
